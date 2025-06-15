2e91ed0951c9880a9b7510a88d8e89ccf23b6703
Pkexec <azertqsdf736@gmail.com>
2025-06-15 04:10:45 -0400

    firstpush

--- exec/builtins/echo.c
+++ exec/builtins/echo.c
@@ -1,63 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   echo.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: abhmidat <abhmidat@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/11 12:27:05 by abhmidat          #+#    #+#             */
-/*   Updated: 2025/06/11 14:56:13 by abhmidat         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minishell.h"
 
-static int	nl_param(char *param)
+void echo(int argc, char **argv) 
 {
-	int	i;
+    int i;
+    int new_line;
 
-	if (!ft_strcmp(param, "-n"))
-		return (1);
-	if (param[0] == '-' && param[1] == 'n')
-	{
-		i = 1;
-		while (param[++i])
-		{
-			if (param[i] != 'n')
-				return (0);
-		}
-		return (1);
-	}
-	return (0);
+    i = 1;
+    new_line = 1;
+    while (i < argc && ft_strcmp(argv[i], "-n") == 0)
+    {
+        newline = 0;
+        i++;
+    }
+    while (i < argc; i++)
+    {
+        printf("%s", argv[i]);
+        if (i < argc - 1)
+            printf(" ");
+    }
+    if (newline)
+        printf("\n");
 }
 
-int	echo(int ac, char **av)
+int main(int ac, char **av)
 {
-	int	i;
-	int	nl;
-	int	first;
-
-	nl = 1;
-	first = 1;
-	i = 0;
-	while (av[++i] && nl_param(av[i]))
-		nl = 0;
-	while (i < ac)
-	{
-		if (!first)
-			ft_putchar_fd(' ', 1);
-		ft_putstr_fd(av[i], 1);
-		if (first)
-			first = 0;
-		++i;
-	}
-	if (ac >= 1 && nl)
-		ft_putchar_fd('\n', 1);
+	echo(ac, av);
 	return (0);
-}
-
-int main(int argc, char **argv)
-{
-    echo(argc, argv);
-    return 0;
 }
