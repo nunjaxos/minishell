b54b1a4e8131b8b6d2c013d4c724c31ed2782f38
xos.nun <abhmidat@DESKTOP-16SMNDK.localdomain>
2025-07-11 15:58:28 +0100

    newss v2

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -1,17 +1,65 @@
 #include "../../include/builtins.h"
 
-void	ft_env(t_env *env, pid_t pid)
+t_env *create_env_node(const char *env)
 {
-	int		i;
-	char	**str;
+	t_env *node;
+	
+	node = malloc(sizeof(t_env));
+	if (!node)
+		return (NULL);
+	char *sep = strchr(env, '=');
+	if (!sep)
+		return (free(node), NULL);
+	node->name = ft_strndup(env, sep - env);
+	if (!node->name)
+		return (free(node), NULL);
+	node->value = strdup(sep + 1);
+	if (!node->value)
+		return (free(node->name), free(node), NULL);
+	node->next = NULL;
+	return node;
+}
 
-	i = 0;
-	str = convert_path_to_array(env);
-	while (str[i])
+void add_env_back(t_env **lst, t_env *new_node)
+{
+	if (!lst || !new_node)
+		return;
+	if (!*lst)
 	{
-		ft_putendl_fd(str[i], 1);
+		*lst = new_node;
+		return;
+	}
+	t_env *temp = *lst;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new_node;
+}
+
+void init_env_list(char **envp, t_data *data)
+{
+	int i = 0;
+	t_env *node;
+
+	data->n_env = NULL;
+	while (envp[i])
+	{
+		node = create_env_node(envp[i]);
+		if (node)
+			add_env_back(&data->n_env, node);
 		i++;
 	}
-	free_char_array(str);
-	check_for_child(pid, 0);
 }
+
+void free_env_list(t_env *env)
+{
+	t_env *tmp;
+	while (env)
+	{
+		tmp = env;
+		env = env->next;
+		free(tmp->name);
+		free(tmp->value);
+		free(tmp);
+	}
+}
+
